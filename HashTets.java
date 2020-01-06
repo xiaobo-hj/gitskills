9a14a99e6696609ef5f6bac9a6d9675c83c50df0
xiaobo-hj <xiaobohj@163.com>
2020-01-06 13:15:51 +0800

    第一次提交

---
+++ HashTets.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+
+public class HashTets {
+    static int[] arr = {0,1,2};
+    public static void main(String[] args) {
+        HashMap<String,String> hashMap = new HashMap<>();
+        hashMap.put("1","1");
+        hashMap.put("2","2");
+
+    }
+}
